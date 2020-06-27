20a17706652558b07d869742781c8636f32e0a27
nachosegalla <58946049+nachosegaalla@users.noreply.github.com>
2020-06-27 16:56:34 -0300

    Agregue entidades Localidad y Provincia

---
+++ TPINT_GRUPO_X_LAB4/src/entidad/Localidad.java
@@ -0,0 +1,35 @@
+package entidad;
+
+public class Localidad {
+
+	private int IdLocalidad;
+	private int IdProvincia;
+	private String Nombre;
+	
+	public int getIdLocalidad() {
+		return IdLocalidad;
+	}
+	
+	public void setIdLocalidad(int idLocalidad) {
+		IdLocalidad = idLocalidad;
+	}
+	
+	public int getIdProvincia() {
+		return IdProvincia;
+	}
+	
+	public void setIdProvincia(int idProvincia) {
+		IdProvincia = idProvincia;
+	}
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	
+	
+	
+}
