80b87346096a3be8662c4eb6b9931797e6ba4d8b
nachosegalla <58946049+nachosegaalla@users.noreply.github.com>
2020-06-27 18:30:00 -0300

    Agregue DaoMateria

---
+++ TPINT_GRUPO_X_LAB4/src/dao/DaoMateria.java
@@ -0,0 +1,48 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entidad.Materia;
+
+public class DaoMateria {
+
+	private static final String readall = "SELECT IdMateria,Nombre FROM materias";
+
+	
+	
+	public List<Materia> readAll()
+	{
+		PreparedStatement statement;
+		ResultSet resultSet;
+		ArrayList<Materia> LMate = new ArrayList<Materia>();
+		Conexion conexion = Conexion.getConexion();
+		try 
+		{
+			statement = conexion.getSQLConexion().prepareStatement(readall);
+			resultSet = statement.executeQuery();
+			while(resultSet.next())
+			{
+				LMate.add(getMateria(resultSet));
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return LMate;
+	}
+
+	private Materia getMateria(ResultSet resultSet) throws SQLException
+	{
+		Materia NMate = new Materia(); 
+		NMate.setIdMateria(resultSet.getInt("IdMateria"));
+		NMate.setNombre(resultSet.getString("Nombre"));
+
+
+		return NMate;
+	}
+}
